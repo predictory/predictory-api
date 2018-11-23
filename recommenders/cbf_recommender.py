6dc8ed9acb08e097538ad4b03cf4a89eb00b6295
Vladimír Fojtík <fojtik.v@gmail.com>
2018-11-23 12:37:48 +0100

    Added item based and user based filtering to app

--- recommenders/cbf_recommender.py
+++ recommenders/cbf_recommender.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from scipy.sparse.linalg import svds
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.neighbors import NearestNeighbors
 from flask_restful import fields, marshal
 from models.user_rating import UserRatingModel
 
@@ -44,6 +46,74 @@ class CBFRecommender():
         predicted_movies_ratings = pd.DataFrame(predicted_ratings.loc[user])
         predicted_movies_ratings.columns = ['rating']
         recommended_movies = predicted_movies_ratings.drop(viewed_movies).sort_values(['rating'], ascending=[0]).head(n)
+        recommendations = {
+            'userId': user,
+            'recommendations': [{index: line.rating} for index, line in recommended_movies.iterrows()]
+        }
+
+        return recommendations
+
+    @staticmethod
+    def ib(user, n):
+        metric = 'cosine'
+
+        users_ratings = marshal(UserRatingModel.query.all(), fields)
+        data = pd.DataFrame(users_ratings)
+        n_users = data['userId'].unique().shape[0]
+        n_items = data['movieId'].unique().shape[0]
+        users = data['userId'].unique()
+        movies = data['movieId'].unique()
+
+        data_matrix = pd.DataFrame(np.zeros((n_users, n_items)), columns=movies, index=users)
+        for line in data.itertuples():
+           data_matrix.at[line.userId, line.movieId] = line.rating
+
+        item_similarity = 1 - pairwise_distances(data_matrix.transpose(), metric=metric)
+
+        pred = data_matrix.values.dot(item_similarity) / np.array([np.abs(item_similarity).sum(axis=1)])
+        item_prediction = pd.DataFrame(pred, columns = movies, index = users)
+
+        user_movies = pd.DataFrame(data_matrix.loc[user])
+        user_movies.columns = ['rating']
+        viewed_movies = user_movies[user_movies['rating'] > 0].index
+        predicted_ratings = pd.DataFrame(item_prediction.loc[user])
+        predicted_ratings.columns = ['rating']
+        recommended_movies = predicted_ratings.drop(viewed_movies).sort_values(['rating'], ascending=[0]).head(n)
+        recommendations = {
+            'userId': user,
+            'recommendations': [{index: line.rating} for index, line in recommended_movies.iterrows()]
+        }
+
+        return recommendations
+
+    @staticmethod
+    def ub(user, n):
+        metric = 'cosine'
+
+        users_ratings = marshal(UserRatingModel.query.all(), fields)
+        data = pd.DataFrame(users_ratings)
+        n_users = data['userId'].unique().shape[0]
+        n_items = data['movieId'].unique().shape[0]
+        users = data['userId'].unique()
+        movies = data['movieId'].unique()
+
+        data_matrix = pd.DataFrame(np.zeros((n_users, n_items)), columns=movies, index=users)
+        for line in data.itertuples():
+           data_matrix.at[line.userId, line.movieId] = line.rating
+
+        user_similarity = 1 - pairwise_distances(data_matrix, metric=metric)
+
+        mean_user_rating = ratings.mean(axis=1)
+        ratings_diff = (ratings - mean_user_rating[:, np.newaxis])
+        pred = mean_user_rating[:, np.newaxis] + similarity.dot(ratings_diff) / np.array([np.abs(similarity).sum(axis=1)]).T
+        user_prediction = pd.DataFrame(pred, columns = movies, index = users)
+
+        user_movies = pd.DataFrame(data_matrix.loc[user])
+        user_movies.columns = ['rating']
+        viewed_movies = user_movies[user_movies['rating'] > 0].index
+        predicted_ratings = pd.DataFrame(item_prediction.loc[user])
+        predicted_ratings.columns = ['rating']
+        recommended_movies = predicted_ratings.drop(viewed_movies).sort_values(['rating'], ascending=[0]).head(n)
         recommendations = {
             'userId': user,
             'recommendations': [{index: line.rating} for index, line in recommended_movies.iterrows()]
