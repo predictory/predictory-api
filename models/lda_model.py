1e9193cb3bdfb8834da8aa4124274d6d553821dc
Vladimír Fojtík <fojtik.v@gmail.com>
2018-12-16 16:42:20 +0100

    Changed lda params

--- models/lda_model.py
+++ models/lda_model.py
@@ -29,13 +29,13 @@ class LDAModel:
         # Min length of document
         self.min_length = 100
         # Num_topics in LDA
-        self.num_topics = 30
+        self.num_topics = 5
         # Filter out tokens that appear in less than `no_below` documents (absolute number)
         self.no_below = 50
         # Filter out tokens that appear in more than `no_above` documents
-        self.no_above = 0.2
+        self.no_above = 0.3
         # Number of iterations in training LDA model
-        self.num_of_iterations = 100000
+        self.num_of_iterations = 10000
 
     @staticmethod
     def lemmatize_stemming(text):
