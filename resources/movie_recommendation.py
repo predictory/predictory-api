1d138f8645294747982c4d19aac88632d9c7ee2d
Vladimír Fojtík <fojtik.v@gmail.com>
2019-06-14 07:58:37 +0200

    Changed get movies method to be static

--- resources/movie_recommendation.py
+++ resources/movie_recommendation.py
@@ -4,7 +4,8 @@ from recommenders.cb_recommender import CBRecommender
 
 
 class MovieRecommendation(Resource):
-    def get(self, movie_id):
+    @staticmethod
+    def get(movie_id):
         take = request.args.get('take', 10, int)
         skip = request.args.get('skip', 0, int)
         recommendations = CBRecommender.get_recommendations(movie_id, take)
