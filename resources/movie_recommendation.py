65e50035990dbb4c4f779ddb1c2a6e561dc0341e
Vladimír Fojtík <fojtik.v@gmail.com>
2019-06-14 07:53:21 +0200

    Added param to set number of returned recommended movies

--- resources/movie_recommendation.py
+++ resources/movie_recommendation.py
@@ -1,8 +1,11 @@
 from flask_restful import Resource
+from flask import request
 from recommenders.cb_recommender import CBRecommender
 
 
 class MovieRecommendation(Resource):
     def get(self, movie_id):
-        recommendations = CBRecommender.get_recommendations(movie_id, 10)
+        take = request.args.get('take', 10, int)
+        skip = request.args.get('skip', 0, int)
+        recommendations = CBRecommender.get_recommendations(movie_id, take)
         return recommendations, 200
