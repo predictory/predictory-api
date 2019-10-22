b29d4fbe2093690a145d2461f13bc42775058476
Fir3st <fojtik.v@gmail.com>
2019-10-22 09:39:42 +0200

    Added ability to sort user recommendations for app

--- resources/user_recommendation.py
+++ resources/user_recommendation.py
@@ -10,6 +10,8 @@ class UserRecommendation(Resource):
         movie_type = request.args.get('type')
         take = request.args.get('take', 10, int)
         skip = request.args.get('skip', 0, int)
+        order_by = request.args.get('order_by', 'rating,es_score')
+        order_by = order_by.split(',')
 
-        recommendations = CBFRecommender.get_recommendations(user_id, take, skip, genres, movie_type)
+        recommendations = CBFRecommender.get_recommendations(user_id, take, skip, genres, movie_type, order_by=order_by)
         return recommendations, 200
