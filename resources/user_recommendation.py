49ac1c0085b999f7dd3dea57c86aee1d82a4d0af
Fir3st <fojtik.v@gmail.com>
2019-10-22 10:05:34 +0200

    Added ability to send fav and not fav genres to user recommendations route

--- resources/user_recommendation.py
+++ resources/user_recommendation.py
@@ -12,6 +12,9 @@ class UserRecommendation(Resource):
         skip = request.args.get('skip', 0, int)
         order_by = request.args.get('order_by', 'rating,es_score')
         order_by = order_by.split(',')
+        fav_genres = request.args.get('fav_genres')
+        not_fav_genres = request.args.get('not_fav_genres')
 
-        recommendations = CBFRecommender.get_recommendations(user_id, take, skip, genres, movie_type, order_by=order_by)
+        recommendations = CBFRecommender.get_recommendations(user_id, take, skip, genres, movie_type, order_by=order_by,
+                                                             fav_genres=fav_genres, not_fav_genres=not_fav_genres)
         return recommendations, 200
