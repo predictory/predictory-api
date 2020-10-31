16904f506047242b2896a5cd0da942b2eadeaf4e
Vladimír Fojtík <fojtik.v@gmail.com>
2020-10-31 15:37:28 +0100

    fixes

--- resources/playground_new.py
+++ resources/playground_new.py
@@ -3,6 +3,7 @@ from models.svd_model import SVDModel
 from utils.database_helper import DatabaseHelper
 import pandas as pd
 from flask import request
+from utils.recommendations_helper import RecommendationsHelper
 
 
 class PlaygroundNew(Resource):
@@ -10,6 +11,7 @@ class PlaygroundNew(Resource):
     def get(user_id):
         svd_model = SVDModel()
         data, movies, users, fav_genres, not_fav_genres = DatabaseHelper.load_data_matrix_limited_by_top_genres(user_id)
+        rated_movies = RecommendationsHelper.get_user_rated_movies(user_id)
         U, sigma, Vt, predicted_ratings = svd_model.train(data, 20)
         ratings_df = pd.DataFrame(predicted_ratings, columns=movies, index=users)
         take = request.args.get('take', 10, int)
@@ -17,12 +19,16 @@ class PlaygroundNew(Resource):
 
         if user_id in ratings_df.index:
             recommendations = ratings_df.loc[user_id].to_dict()
+            for movie in rated_movies:
+                try:
+                    del recommendations[movie['movieId']]
+                except:
+                    print('Movie not found')
             recommendations = [{'id': k, 'rating': v} for k, v in sorted(recommendations.items(), key=lambda item: item[1], reverse=True)]
 
             # boost rating for items from favourite genres
             # boost rating for 5 top items from genre
             # reduce rating for items from not favourites genres
-
 
             return recommendations[skip:take], 200
         else:
